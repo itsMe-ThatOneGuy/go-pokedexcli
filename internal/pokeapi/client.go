e5834205d7079f079e49a93c0fb3341d97bb75f4
Matthew Smart <matthewsmart13@gmail.com>
2024-11-09 12:27:06 -0600

    Added pokecache to the client

--- internal/pokeapi/client.go
+++ internal/pokeapi/client.go
@@ -3,14 +3,18 @@ package pokeapi
 import (
 	"net/http"
 	"time"
+
+	"github.com/itsMe-ThatOneGuy/go-pokedexcli/internal/pokecache"
 )
 
 type Client struct {
+	cache      pokecache.Cache
 	httpClient http.Client
 }
 
 func NewClient() Client {
 	return Client{
+		cache: pokecache.NewCache(),
 		httpClient: http.Client{
 			Timeout: 5 * time.Second,
 		},
