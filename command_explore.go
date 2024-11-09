1f02012898e0e8ed3957750fc94dfe1f1dc9eab3
Matthew Smart <matthewsmart13@gmail.com>
2024-11-09 17:45:37 -0600

    Created the explore command

---
+++ command_explore.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func commandExplore(conf *config, args ...string) error {
+	if len(args) == 0 {
+		return errors.New("You need to provide a location name")
+	}
+
+	name := args[0]
+	location, err := conf.pokeapiClient.GetLocation(name)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Exploring %s...\n", name)
+	fmt.Println("Pokemon Found:")
+	for _, pokemon := range location.PokemonEncounters {
+		fmt.Printf(" - %s\n", pokemon.Pokemon.Name)
+	}
+	return nil
+}
