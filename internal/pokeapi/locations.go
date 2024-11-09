15bb33fcd05ac713bf082085eb8a7bb0b2e4ae9f
Matthew Smart <matthewsmart13@gmail.com>
2024-11-09 10:21:08 -0600

    created logic of pokeapi locations request

---
+++ internal/pokeapi/locations.go
@@ -0,0 +1,48 @@
+package pokeapi
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+type Locations struct {
+	Count    int     `json:"count"`
+	Next     *string `json:"next"`
+	Previous *string `json:"Previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		Url  string `json:"url"`
+	} `json:"results"`
+}
+
+func (c *Client) ListLocations(page *string) (Locations, error) {
+	url := baseUrl
+	if page != nil {
+		url = *page
+	}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return Locations{}, nil
+	}
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return Locations{}, nil
+	}
+	defer res.Body.Close()
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return Locations{}, nil
+	}
+
+	locationRes := Locations{}
+	err = json.Unmarshal(data, &locationRes)
+	if err != nil {
+		return Locations{}, nil
+	}
+
+	return locationRes, nil
+}
