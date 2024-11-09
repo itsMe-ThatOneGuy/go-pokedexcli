b84ec9663512cac59dbf8c2f9ff0c7219ee54d75
Matthew Smart <matthewsmart13@gmail.com>
2024-11-09 17:47:02 -0600

    Added the GetLocation func

--- internal/pokeapi/locations.go
+++ internal/pokeapi/locations.go
@@ -6,6 +6,44 @@ import (
 	"net/http"
 )
 
+func (c *Client) GetLocation(locationName string) (Location, error) {
+	url := baseUrl + "/location-area/" + locationName
+
+	if val, ok := c.cache.Get(url); ok {
+		locationRes := Location{}
+		err := json.Unmarshal(val, &locationRes)
+		if err != nil {
+			return Location{}, nil
+		}
+		return locationRes, nil
+	}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return Location{}, err
+	}
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return Location{}, err
+	}
+	defer res.Body.Close()
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return Location{}, err
+	}
+
+	locationRes := Location{}
+	err = json.Unmarshal(data, &locationRes)
+	if err != nil {
+		return Location{}, nil
+	}
+
+	c.cache.Add(url, data)
+	return locationRes, nil
+}
+
 func (c *Client) ListLocations(page *string) (LocationsList, error) {
 	url := baseUrl + "/location-area"
 	if page != nil {
