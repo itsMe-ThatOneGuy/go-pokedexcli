414c33e6cab2dc5d7e97405a4578be05c01a7f2f
Matthew Smart <matthewsmart13@gmail.com>
2024-11-09 22:27:14 -0600

    Added new Println

--- command_help.go
+++ command_help.go
@@ -8,6 +8,7 @@ func commandHelp(conf *config, args ...string) error {
 	fmt.Println()
 	fmt.Println("*Beep* Booting Pokedex *Boop*")
 	fmt.Println("Usage:")
+	fmt.Println()
 	for _, cmd := range commands() {
 		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
 	}
